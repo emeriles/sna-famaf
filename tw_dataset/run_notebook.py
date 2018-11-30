d68dde1554853d94f6d2b5cdc3f17c55c9cbd502
Emanuel Meriles <emeriles@gmail.com>
2018-11-30 12:26:38 -0300

    added timing and raised timeout for cell

--- tw_dataset/run_notebook.py
+++ tw_dataset/run_notebook.py
@@ -3,6 +3,7 @@ import codecs
 import nbformat as nbf
 from nbconvert.preprocessors import ExecutePreprocessor
 from os.path import join
+import time
 
 
 def generate_notebook(dataset_path):
@@ -11,7 +12,7 @@ def generate_notebook(dataset_path):
     print("Computing %s" % out_nb_fname)
     nb = nbf.read(open(notebook_name), as_version=4)
     nb['cells'][0]['source'] = 'csv_path ="{}"'.format(dataset_path)
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep = ExecutePreprocessor(timeout=1200, kernel_name='python3')
     ep.preprocess(nb, {'metadata': {'path': './'}})
     with codecs.open(out_nb_fname, 'w', encoding='utf-8') as f:
         nbf.write(nb, f)
@@ -20,5 +21,8 @@ def generate_notebook(dataset_path):
 if __name__ == '__main__':
     dataset_paths = ['../../database/dayli_collections/dayli_col.csv']
     for dataset_path in dataset_paths:
+        start_time = time.time()
         print("Running for {}".format(dataset_path))
         generate_notebook(dataset_path=dataset_path)
+        run_time = time.time() - start_time
+        print('Time to run {} was {}'.format(dataset_path, run_time))
